4560331a6a8a74642609a7b9bec74a6b6b968c6e
Dylan <dylanrose60@gmail.com>
2017-05-28 12:19:07 -0400

    responsive

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 $(document).ready( function() {
-  alignContent();
+  if( $(window).width() >= 1100 )
+    alignContent();
+  else {
+    var contentTopMargin = $('nav').outerHeight() + $('nav').find('.col').outerHeight() + 50;
+    $('#landing-rest').css('margin-top', contentTopMargin);
+  }
 
   $(window).resize( function() {
-    alignContent();
+    //alignContent();
+
+    if( $(window).width() >= 1100 ) {
+      $('#landing-rest').css( 'margin-top', '1.5em' )
+      alignContent()
+    } else {
+      var contentTopMargin = $('nav').outerHeight() + $('nav').find('.col').outerHeight() + 50;
+      $('#landing-rest').css('margin-top', contentTopMargin);
+      $('#landing-rest').css('margin-left', '0');
+    }
+
   } );
 
   function alignContent() {
     var contentLeftMargin = $('nav').offset().left + $('nav').outerWidth();
     $('#landing-rest').css( 'margin-left', contentLeftMargin );
-    $('#test').css('margin-left', contentLeftMargin);
   }
 });
